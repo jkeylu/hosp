60d2786764b66949152a6544fe272f9359dd6991
jKey Lu <jkeylu@gmail.com>
2016-05-15 22:35:49 +0800

    optimize code

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"golang.org/x/net/proxy"
@@ -9,6 +11,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 const VERSION = "v1.0.0"
@@ -48,107 +51,140 @@ func (server *Server) ListenAndServe() {
 	}
 }
 
+func hash(req *http.Request) string {
+	str := time.Now().String() + req.Method + req.URL.String()
+	hash := md5.Sum([]byte(str))
+	return hex.EncodeToString(hash[:])
+}
+
+type Client struct {
+	id  string
+	w   http.ResponseWriter
+	req *http.Request
+}
+
 func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Printf("%s %s\n", req.Method, req.Host)
+	id := hash(req)
+	client := &Client{id, w, req}
+
+	if server.verbose {
+		log.Printf("%s: %s %s\n", id, req.Method, req.URL.String())
+	} else {
+		log.Printf("%s: %s %s\n", id, req.Method, req.Host)
+	}
 
 	var err error
 	if req.Method == http.MethodConnect {
-		err = server.tunnel(w, req)
+		err = server.tunnel(client)
 
 	} else {
-		err = server.transfer(w, req)
+		err = server.transfer(client)
 	}
 
 	if err != nil {
-		if server.verbose {
-			log.Println(err)
-		}
+		log.Printf("%s: \n%v", id, err)
+	}
+
+	if server.verbose {
+		log.Println(id, "transferring finished")
 	}
 }
 
-func (server *Server) tunnel(w http.ResponseWriter, req *http.Request) error {
-	var err error
-
-	remoteConn, err := server.socks5Dialer.Dial("tcp", req.Host)
-	if err != nil {
+func establishConnection(conn net.Conn) error {
+	if _, err := fmt.Fprint(conn, "HTTP/1.1 200 Connection established\r\n"); err != nil {
 		return err
 	}
-
-	hijacker, ok := w.(http.Hijacker)
-	if !ok {
-		return fmt.Errorf("")
-	}
-
-	clientConn, _, err := hijacker.Hijack()
-	if err != nil {
+	if _, err := fmt.Fprint(conn, "Connection: close\r\n"); err != nil {
 		return err
 	}
-
-	done := make(chan error)
-
-	go server.pipe(clientConn, remoteConn, done)
-	go server.pipe(remoteConn, clientConn, done)
-
-	if _, err := fmt.Fprint(clientConn, "HTTP/1.1 200 Connection established\r\n"); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprint(clientConn, "Connection: close\r\n"); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprint(clientConn, "\r\n"); err != nil {
-		return err
-	}
-
-	for waiting := 2; waiting > 0; {
-		select {
-		case err = <-done:
-			waiting--
-		}
-		if err != nil {
-			break
-		}
-	}
-
-	close(done)
-	if err != nil {
+	if _, err := fmt.Fprint(conn, "\r\n"); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (server *Server) pipe(src, dst net.Conn, done chan<- error) {
-	_, err := io.Copy(dst, src)
-	if err != nil {
-		log.Println(err)
+func (server *Server) tunnel(client *Client) error {
+	var err error
+	var remoteConn net.Conn
+	var clientConn net.Conn
+
+	if remoteConn, err = server.socks5Dialer.Dial("tcp", client.req.Host); err != nil {
+		return err
 	}
-	done <- nil
+	defer remoteConn.Close()
+
+	hi, ok := client.w.(http.Hijacker)
+	if !ok {
+		return fmt.Errorf("not support hijacking")
+	}
+
+	if clientConn, _, err = hi.Hijack(); err != nil {
+		return err
+	}
+	defer clientConn.Close()
+
+	errCh := make(chan error, 2)
+	defer close(errCh)
+
+	go func() {
+		written, err := io.Copy(clientConn, remoteConn)
+		if err == nil && server.verbose {
+			log.Printf("%s: %s %d", client.id, "client <- remote", written)
+		}
+		errCh <- err
+	}()
+	go func() {
+		written, err := io.Copy(remoteConn, clientConn)
+		if err == nil && server.verbose {
+			log.Printf("%s: %s %d", client.id, "client -> remote", written)
+		}
+		errCh <- err
+	}()
+
+	if err := establishConnection(clientConn); err != nil {
+		return err
+	}
+
+	for i := 0; i < 2; i++ {
+		select {
+		case err = <-errCh:
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
-func (server *Server) transfer(w http.ResponseWriter, req *http.Request) error {
-	req.Header.Del("Proxy-Connection")
-	req.Header.Del("Connection")
-	req.Header.Del("Keep-Alive")
+func (server *Server) transfer(client *Client) error {
+	client.req.Header.Del("Proxy-Connection")
+	client.req.Header.Del("Connection")
+	client.req.Header.Del("Keep-Alive")
 
 	transport := &http.Transport{
 		Dial: server.socks5Dialer.Dial,
 	}
 
-	res, err := transport.RoundTrip(req)
+	res, err := transport.RoundTrip(client.req)
 	if err != nil {
 		return err
 	}
 
 	for key, values := range res.Header {
 		for _, value := range values {
-			w.Header().Add(key, value)
+			client.w.Header().Add(key, value)
 		}
 	}
-	w.WriteHeader(res.StatusCode)
+	client.w.WriteHeader(res.StatusCode)
 
-	_, err = io.Copy(w, res.Body)
+	written, err := io.Copy(client.w, res.Body)
 	if err != nil {
 		return err
+	}
+	if server.verbose {
+		log.Printf("%s %s %d", client.id, "transfer", written)
 	}
 	return nil
 }
