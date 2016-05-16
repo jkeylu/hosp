8d2d6d6f15f74988b289a47d4a45688d05c4eca2
jKey Lu <jkeylu@gmail.com>
2016-05-16 23:30:17 +0800

    add whitelist support

--- main.go
+++ main.go
@@ -2,27 +2,92 @@ package main
 
 import (
 	"crypto/md5"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"golang.org/x/net/proxy"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
-const VERSION = "v1.0.0"
+const VERSION = "1.0.0"
 
-func handleCommandLine() (localAddr string, remoteAddr string, verbose bool, version bool) {
-	flag.StringVar(&localAddr, "l", "127.0.0.1:8080", "set http proxy 'host:port'")
-	flag.StringVar(&remoteAddr, "s", "127.0.0.1:1080", "set remote socks5 server 'host:port'")
-	flag.BoolVar(&verbose, "verbose", false, "print extra debuging information")
-	flag.BoolVar(&version, "v", false, "display version")
-	flag.Parse()
-	return
+type WhiteList struct {
+	filepath string
+	list     []string
+}
+
+func NewWhiteList(filepath string) *WhiteList {
+	var wl = &WhiteList{filepath: filepath}
+	wl.list = make([]string, 0)
+
+	if filepath == "" {
+		return wl
+	}
+
+	data, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		log.Printf("read file \"%s\" error\n%v", filepath, err)
+		return wl
+	}
+
+	list := strings.Split(string(data), "\n")
+
+	for _, host := range list {
+		if host != "" && !wl.has(host) {
+			wl.list = append(wl.list, host)
+		}
+	}
+
+	log.Printf("white list count: %d\n", len(wl.list))
+	return wl
+}
+
+func (wl *WhiteList) has(host string) bool {
+	return wl.indexOf(host) >= 0
+}
+
+func (wl *WhiteList) indexOf(host string) int {
+	for i, h := range wl.list {
+		if strings.Contains(host, h) {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (wl *WhiteList) add(host string) {
+	if wl.has(host) {
+		return
+	}
+
+	log.Printf("add new host \"%s\"\n", host)
+
+	wl.list = append(wl.list, host)
+	wl.saveToFile()
+}
+
+func (wl *WhiteList) remove(host string) {
+	i := wl.indexOf(host)
+	if i < 0 {
+		return
+	}
+
+	log.Printf("remove host \"%s\"\n", host)
+
+	wl.list = append(wl.list[:i], wl.list[i+1:]...)
+	wl.saveToFile()
+}
+
+func (wl *WhiteList) saveToFile() {
+	data := []byte(strings.Join(wl.list, "\n"))
+	ioutil.WriteFile(wl.filepath, data, 0644)
 }
 
 type Server struct {
@@ -30,6 +95,7 @@ type Server struct {
 	remoteAddr   string
 	socks5Dialer proxy.Dialer
 	verbose      bool
+	whitelist    *WhiteList
 }
 
 func (server *Server) ListenAndServe() {
@@ -51,14 +117,13 @@ func (server *Server) ListenAndServe() {
 	}
 }
 
-func hash(req *http.Request) string {
+func hash(req *http.Request) [16]byte {
 	str := time.Now().String() + req.Method + req.URL.String()
-	hash := md5.Sum([]byte(str))
-	return hex.EncodeToString(hash[:])
+	return md5.Sum([]byte(str))
 }
 
 type Client struct {
-	id  string
+	id  [16]byte
 	w   http.ResponseWriter
 	req *http.Request
 }
@@ -68,9 +133,9 @@ func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	client := &Client{id, w, req}
 
 	if server.verbose {
-		log.Printf("%s: %s %s\n", id, req.Method, req.URL.String())
+		log.Printf("%x %s %s\n", id, req.Method, req.URL.String())
 	} else {
-		log.Printf("%s: %s %s\n", id, req.Method, req.Host)
+		log.Printf("%x %s %s\n", id, req.Method, req.Host)
 	}
 
 	var err error
@@ -82,11 +147,11 @@ func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 
 	if err != nil {
-		log.Printf("%s: \n%v", id, err)
+		log.Printf("%x \n%v", id, err)
 	}
 
 	if server.verbose {
-		log.Println(id, "transferring finished")
+		log.Printf("%x transferring finished", id)
 	}
 }
 
@@ -109,20 +174,29 @@ func (server *Server) tunnel(client *Client) error {
 	var remoteConn net.Conn
 	var clientConn net.Conn
 
-	if remoteConn, err = server.socks5Dialer.Dial("tcp", client.req.Host); err != nil {
-		return err
+	host := client.req.Host
+	if server.whitelist.has(host) {
+		remoteConn, err = net.Dial("tcp", host)
+		log.Printf("%x direct connect \"%s\"\n", client.id, host)
+
+	} else {
+		remoteConn, err = server.socks5Dialer.Dial("tcp", host)
 	}
 	defer remoteConn.Close()
+	if err != nil {
+		return err
+	}
 
 	hi, ok := client.w.(http.Hijacker)
 	if !ok {
 		return fmt.Errorf("not support hijacking")
 	}
 
-	if clientConn, _, err = hi.Hijack(); err != nil {
+	clientConn, _, err = hi.Hijack()
+	defer clientConn.Close()
+	if err != nil {
 		return err
 	}
-	defer clientConn.Close()
 
 	errCh := make(chan error, 2)
 	defer close(errCh)
@@ -130,14 +204,14 @@ func (server *Server) tunnel(client *Client) error {
 	go func() {
 		written, err := io.Copy(clientConn, remoteConn)
 		if err == nil && server.verbose {
-			log.Printf("%s: %s %d", client.id, "client <- remote", written)
+			log.Printf("%x %s %d", client.id, "client <- remote", written)
 		}
 		errCh <- err
 	}()
 	go func() {
 		written, err := io.Copy(remoteConn, clientConn)
 		if err == nil && server.verbose {
-			log.Printf("%s: %s %d", client.id, "client -> remote", written)
+			log.Printf("%x %s %d", client.id, "client -> remote", written)
 		}
 		errCh <- err
 	}()
@@ -163,9 +237,17 @@ func (server *Server) transfer(client *Client) error {
 	client.req.Header.Del("Connection")
 	client.req.Header.Del("Keep-Alive")
 
-	transport := &http.Transport{
-		Dial: server.socks5Dialer.Dial,
+	host := client.req.Host
+	var dial func(network, addr string) (net.Conn, error)
+	if server.whitelist.has(host) {
+		dial = nil
+		log.Printf("%x direct %s \"%s\"\n", client.id, client.req.Method, host)
+
+	} else {
+		dial = server.socks5Dialer.Dial
 	}
+
+	transport := &http.Transport{Dial: dial}
 
 	res, err := transport.RoundTrip(client.req)
 	if err != nil {
@@ -184,13 +266,23 @@ func (server *Server) transfer(client *Client) error {
 		return err
 	}
 	if server.verbose {
-		log.Printf("%s %s %d", client.id, "transfer", written)
+		log.Printf("%x %s %d", client.id, "transfer", written)
 	}
 	return nil
 }
 
+func handleCommandLine() (localAddr, remoteAddr, wlFile string, verbose, version bool) {
+	flag.StringVar(&localAddr, "l", "127.0.0.1:8080", "set http proxy 'host:port'")
+	flag.StringVar(&remoteAddr, "s", "127.0.0.1:1080", "set remote socks5 server 'host:port'")
+	flag.StringVar(&wlFile, "w", "", "set host white list file path")
+	flag.BoolVar(&verbose, "verbose", false, "print extra debuging information")
+	flag.BoolVar(&version, "v", false, "display version")
+	flag.Parse()
+	return
+}
+
 func main() {
-	localAddr, remoteAddr, verbose, version := handleCommandLine()
+	localAddr, remoteAddr, wlFile, verbose, version := handleCommandLine()
 
 	if flag.NFlag() == 0 && flag.NArg() > 0 {
 		flag.Usage()
@@ -198,7 +290,7 @@ func main() {
 	}
 
 	if version {
-		fmt.Println(VERSION)
+		log.Printf("v%s\n", VERSION)
 		return
 	}
 
@@ -206,6 +298,7 @@ func main() {
 		localAddr:  localAddr,
 		remoteAddr: remoteAddr,
 		verbose:    verbose,
+		whitelist:  NewWhiteList(wlFile),
 	}
 
 	server.ListenAndServe()
